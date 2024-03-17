655f2026173bf98b3b7797b71c386d3e7c84e226
Facundo Talavera <Facundo_2112@outlook.com>
2024-03-17 11:57:42 -0300

    commit

---
+++ vite.config.mjs
@@ -0,0 +1,22 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
+
+export default defineConfig({
+
+  plugins: [glsl({
+  include: [                   
+    '**/*.glsl', '**/*.wgsl',
+    '**/*.vert', '**/*.frag',
+    '**/*.vs', '**/*.fs'
+  ],
+  exclude: undefined,         
+  warnDuplicatedImports: true, 
+  defaultExtension: 'glsl',   
+  compress: false,            
+  watch: true,                 
+  root: '/',
+
+})]
+});
